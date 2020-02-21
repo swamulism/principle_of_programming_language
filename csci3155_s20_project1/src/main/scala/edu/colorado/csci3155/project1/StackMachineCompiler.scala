440b5f7eb704ca2c39eff65328ed96237a596cec
Samuel Eubanks <swamulism@gmail.com>
2020-02-21 15:07:48 -0700

    Finish homework perhaps, tests seem to be broken

--- csci3155_s20_project1/src/main/scala/edu/colorado/csci3155/project1/StackMachineCompiler.scala
+++ csci3155_s20_project1/src/main/scala/edu/colorado/csci3155/project1/StackMachineCompiler.scala
@@ -7,5 +7,15 @@ object StackMachineCompiler {
         The type of stackmachine instructions are in the file StackMachineEmulator.scala in this same directory
         The type of Expr is in the file Expr.scala in this directory.
      */
-    def compileToStackMachineCode(e: Expr): List[StackMachineInstruction] = ???
+    def compileToStackMachineCode(e: Expr): List[StackMachineInstruction] = e match {
+        case Const(f) => List(PushI(f))
+        case Plus(e1, e2) => compileToStackMachineCode(e1) ++ compileToStackMachineCode(e2) :+ AddI
+        case Minus(e1, e2) => compileToStackMachineCode(e1) ++ compileToStackMachineCode(e2) :+ SubI
+        case Mult(e1, e2) => compileToStackMachineCode(e1) ++ compileToStackMachineCode(e2) :+ MultI
+        case Div(e1, e2) => compileToStackMachineCode(e1) ++ compileToStackMachineCode(e2) :+ DivI
+        case Exp(e) => compileToStackMachineCode(e) :+ ExpI
+        case Log(e) => compileToStackMachineCode(e) :+ LogI
+        case Sine(e) => compileToStackMachineCode(e) :+ SinI
+        case Cosine(e) => compileToStackMachineCode(e) :+ CosI
+    }
 }
--- csci3155_s20_project1/src/main/scala/edu/colorado/csci3155/project1/StackMachineEmulator.scala
+++ csci3155_s20_project1/src/main/scala/edu/colorado/csci3155/project1/StackMachineEmulator.scala
@@ -24,9 +24,67 @@ object StackMachineEmulator {
         Throw an exception or assertion violation when error happens.
 
      */
-    def emulateSingleInstruction(stack: List[Double], ins: StackMachineInstruction): List[Double] =
-        ???
+    def emulateSingleInstruction(stack: List[Double], ins: StackMachineInstruction): List[Double] = ins match {
+        case AddI => {
+            checkStackSize(stack,2)
+            applyInstruction(stack, _+_)
+        }
+        case SubI => {
+            checkStackSize(stack,2)
+            applyInstruction(stack, _-_)
+        }
+        case MultI => {
+            checkStackSize(stack,2)
+            applyInstruction(stack, _*_)
+        }
+        case DivI => {
+            checkStackSize(stack,2)
+            if (stack.last == 0) {
+                throw new Exception("Divide by zero")
+            }
+            applyInstruction(stack, _/_)
+        }
+        case ExpI => {
+            checkStackSize(stack,1)
+            applyInstruction(stack, math.exp(_))
+        }
+        case LogI => {
+            checkStackSize(stack,1)
+            if (stack.last <= 0) {
+                throw new Exception("Log of non positive")
+            }
+            applyInstruction(stack, math.log(_))
+        }
+        case SinI => {
+            checkStackSize(stack,1)
+            applyInstruction(stack, math.sin(_))
+        }
+        case CosI => {
+            checkStackSize(stack,1)
+            applyInstruction(stack, math.cos(_))
+        }
+        case PushI(f) => stack :+ f
+        case PopI => {
+            checkStackSize(stack,1)
+            stack.init
+        }
+    }
 
+    def checkStackSize(stack: List[Double], size: Int) = {
+        if (stack.length < size) {
+            throw new Exception("Stack too small")
+        }
+    }
+
+    def applyInstruction(stack: List[Double], f: (Double, Double) => Double): List[Double] = {
+        val (left, right) = stack.splitAt(stack.length - 2)
+        left :+ f(right(0), right(1))
+    }
+
+    def applyInstruction(stack: List[Double], f: Double => Double): List[Double] = {
+        val (left, right) = stack.splitAt(stack.length - 1)
+        left :+ f(right(0))
+    }
     /* Function emulateStackMachine
        Execute the list of instructions provided as inputs using the
        emulateSingleInstruction function.
@@ -35,6 +93,6 @@ object StackMachineEmulator {
        are executed.
      */
     def emulateStackMachine(instructionList: List[StackMachineInstruction]): Double =
-        ???
+        instructionList.foldLeft(List[Double]())(emulateSingleInstruction(_, _)).last
 
 }

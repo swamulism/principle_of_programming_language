e1224fb840be6cbb87c86c6ad7f8e5e22128a205
Samuel Eubanks <swamulism@gmail.com>
2020-03-30 17:23:59 -0600

    Start miniproj 2

--- csci3155_s20_project2/src/main/scala/edu/colorado/csci3155/project2/MyCanvas.scala
+++ csci3155_s20_project2/src/main/scala/edu/colorado/csci3155/project2/MyCanvas.scala
@@ -22,10 +22,18 @@ sealed trait Figure {
 case class Polygon(val cList: List[(Double, Double)]) extends Figure {
     //TODO: Define the bounding box of the polygon
     //      A bounding box is a 4-tuple (xMin, xMax, yMin, yMax)
-    override def getBoundingBox: (Double, Double, Double, Double ) = ???
+    override def getBoundingBox: (Double, Double, Double, Double ) = {
+        val xMin = cList.map(elt => elt._1).min
+        val xMax = cList.map(elt => elt._1).max
+        val yMin = cList.map(elt => elt._2).min
+        val yMax = cList.map(elt => elt._2).max
+        (xMin, xMax, yMin, yMax)
+    }
     //TODO: Create a new polygon by shifting each vertex in cList by (x,y)
     //    Do not change the order in which the vertices appear
-    override def translate(shiftX: Double, shiftY: Double): Polygon = ???
+    override def translate(shiftX: Double, shiftY: Double): Polygon = {
+        Polygon(cList.map((x) => (x._1 + shiftX, x._2 + shiftY)))
+    }
 
     // Function: render -- draw the polygon. Do not edit this function.
     override def render(g: Graphics2D, scaleX: Double, scaleY: Double, shiftX: Double, shiftY: Double) = {
@@ -46,11 +54,18 @@ case class Polygon(val cList: List[(Double, Double)]) extends Figure {
 case class MyCircle(val c: (Double, Double), val r: Double) extends Figure {
     //TODO: Define the bounding box for the circle
     //    A bounding box is a 4-tuple (xMin, xMax, yMin, yMax)
-    override def getBoundingBox: (Double, Double, Double, Double) = ???
-
+    override def getBoundingBox: (Double, Double, Double, Double) = {
+        val xMin = c._1 - r
+        val xMax = c._1 + r
+        val yMin = c._2 - r
+        val yMax = c._2 + r
+        (xMin, xMax, yMin, yMax)
+    }
 
     //TODO: Create a new circle by shifting the center
-    override def translate(shiftX: Double, shiftY: Double): MyCircle = ???
+    override def translate(shiftX: Double, shiftY: Double): MyCircle = {
+        MyCircle((c._1 + shiftX, c._2 + shiftY), r)
+    }
 
     // Function: render -- draw the polygon. Do not edit this function.
     override def render(g: Graphics2D, scaleX: Double, scaleY: Double, shiftX: Double, shiftY: Double) = {
@@ -71,20 +86,41 @@ class MyCanvas (val listOfObjects: List[Figure]) {
     // TODO: Write a function to get the boundingbox for the entire canvas.
     //     Hint: use existing getBoundingBox functions defined in each figure.
     //     A bounding box is defined as (xMin, xMax, yMin, yMax)
-    def getBoundingBox: (Double, Double, Double, Double) = ???
+    def getBoundingBox: (Double, Double, Double, Double) = {
+        val boxes = listOfObjects.map(x => x.getBoundingBox)
+        val xMin = boxes.map(elt => elt._1).min
+        val xMax = boxes.map(elt => elt._2).max
+        val yMin = boxes.map(elt => elt._3).min
+        val yMax = boxes.map(elt => elt._4).max
+        (xMin, xMax, yMin, yMax)
+    }
 
     //TODO: Write a function to translate each figure in the canvas by shiftX, shiftY
-    def translate(shiftX: Double, shiftY: Double): MyCanvas = ???
+    def translate(shiftX: Double, shiftY: Double): MyCanvas = {
+        new MyCanvas(listOfObjects.map(x => x.translate(shiftX, shiftY)))
+    }
 
     //TODO: Write a function that will return a new MyCanvas object that places
     // all the objects in myc2 to the right of the objects in this MyCanvas.
     // refer to the notebook documentation on how to perform this.
-    def placeRight(myc2: MyCanvas):MyCanvas = ???
+    def placeRight(myc2: MyCanvas):MyCanvas = {
+        val (xMin1, xMax1, yMin1, yMax1) = getBoundingBox
+        val (xMin2, xMax2, yMin2, yMax2) = myc2.getBoundingBox
+        val xShift = xMax1 - xMin1
+        val yShift = (yMax1 - yMin1) / 2 - (yMax2 - yMin2) / 2
+        overlap(myc2.translate(xShift, yShift))
+    }
 
     //TODO: Write a function that will return a new MyCanvas object that places
     // all the figures in myc2 on top of the figures in this MyCanvas.
     // refer to the notebook documentation on how to perform this.
-    def placeTop(myc2: MyCanvas): MyCanvas = ???
+    def placeTop(myc2: MyCanvas): MyCanvas = {
+        val (xMin1, xMax1, yMin1, yMax1) = getBoundingBox
+        val (xMin2, xMax2, yMin2, yMax2) = myc2.getBoundingBox
+        val xShift = (xMax1 - xMin1) / 2 - (xMax2 - xMin2) / 2
+        val yShift = yMax1 - yMin1
+        overlap(myc2.translate(xShift, yShift))
+    }
 
     //TODO: Write a function that will rotate each figure in the canvas using
     // the angle `ang` defined in radians.
